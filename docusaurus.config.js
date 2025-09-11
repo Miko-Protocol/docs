251551bcf8dd777850eeefbfcec710a32db00048
Hyeonsoo Lee <humiccoils@protonmail.com>
2025-09-12 01:00:05 +0900

    v 1.4.1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,10 +14,10 @@ const config = {
   favicon: 'img/logo_2.png',
 
   // Set the production url of your site here
-  url: 'https://Miko-Protocol.github.io',
+  url: 'https://docs.mikoprotocol.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   organizationName: 'Miko-Protocol', // Usually your GitHub org/user name.
